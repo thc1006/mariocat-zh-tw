d36d938a01d2359bef0014f993d887645b152797
thc1006 <84045975+thc1006@users.noreply.github.com>
2026-05-06 02:25:04 +0800

    fix(web): OFFSCREENCANVASES_TO_PTHREAD=#canvas（解 transferredCanvasNames not iterable）
    OFFSCREEN_FRAMEBUFFER + OFFSCREENCANVAS_SUPPORT 兩個只是『開能力』，
    但要實際把 #canvas transfer 給 worker thread 還需要明示
    OFFSCREENCANVASES_TO_PTHREAD=#canvas 旗標。沒加的話 emscripten
    runtime 在 click handler 內讀 Module.transferredCanvasNames 是
    undefined，於是丟『not iterable』TypeError。

--- scripts/build-web.sh
+++ scripts/build-web.sh
@@ -70,6 +70,7 @@ MSYS_NO_PATHCONV=1 MSYS2_ARG_CONV_EXCL='*' docker run --rm \
         -s PROXY_TO_PTHREAD=1 \
         -s OFFSCREEN_FRAMEBUFFER=1 \
         -s OFFSCREENCANVAS_SUPPORT=1 \
+        -s "OFFSCREENCANVASES_TO_PTHREAD=#canvas" \
         -s ALLOW_MEMORY_GROWTH=1 \
         -s INITIAL_MEMORY=128MB \
         -pthread \
